1a3db5c4fa234f75b150be2588d803f025ce6087
Dmitrii Rogozin <dmitrii@vaadin.com>
2015-05-06 16:41:02 +0300

    fix bug with wrong db.config path

--- src/main/java/studio/MainClass.java
+++ src/main/java/studio/MainClass.java
@@ -1,19 +1,21 @@
 package studio;
 
-import java.io.File;
-
 import com.orientechnologies.orient.server.OServer;
 import com.orientechnologies.orient.server.OServerMain;
 
 public class MainClass {
 
 	public static void main(String[] args) throws Exception {
+		MainClass mainClass = new MainClass();
+		mainClass.startServer();
+	}
+
+	public void startServer() throws Exception {
 		OServer server = OServerMain.create();
-		server.startup(new File("db.config"));
+		server = server.startup(getClass().getResourceAsStream("db.config"));
 		server.activate();
 
 		System.out.println("Server started");
 		System.in.read();
 	}
-
 }
